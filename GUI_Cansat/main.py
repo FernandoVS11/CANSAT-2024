f05bf433486c2d50c50b8dcb9743539e652b2a23
Fernando <fernandvillajuana123@gmail.com>
2024-04-10 15:41:07 -0600

    Arduino
    Correcciones finales, aun falta modularizar

--- GUI_Cansat/main.py
+++ GUI_Cansat/main.py
@@ -22,11 +22,13 @@ tiempo_inicio = time.time()
 class VentanaPrincipal(QMainWindow):
 	velocidad_si ='m/s'
 	aceleracion_si ='m/s^2'
-	presion= 'pa'
+	presion_si= 'pa'
 	grados_si='°'
 	distancia_si='m'
 	temperatura_si ='°C'
 	radio_tierra= 6378
+	dato_arduino_estatico =[]
+	data_from_arduino_estatico_antena_2 = []
 	def __init__(self):
 		super(VentanaPrincipal,self).__init__()
 		loadUi('Diseño.ui',self)
@@ -72,8 +74,20 @@ class VentanaPrincipal(QMainWindow):
 			print('no entra')
 
 		self.serial.readyRead.connect(self.read_data)
+		
 		self.x = list(np.linspace(0,100,100))
-		self.y = list(np.linspace(0,0,100))
+		self.altura_data = list(np.linspace(0,0,100))
+		self.presion_data = list(np.linspace(0,0,100))
+		self.temperatura_data = list(np.linspace(0,0,100))
+		self.yaw = list(np.linspace(0,0,100))
+		self.pitch = list(np.linspace(0,0,100))
+		self.roll = list(np.linspace(0,0,100))
+		self.aceleracion_x = list(np.linspace(0,0,100))
+		self.aceleracion_y = list(np.linspace(0,0,100))
+		self.aceleracion_z = list(np.linspace(0,0,100))
+		self.latitud = list(np.linspace(0,0,100))
+		self.longitud = list(np.linspace(0,0,100))
+		
 		self.z = list(np.linspace(0,0,100))
 
 		self.gfc_temperatura = pg.PlotWidget(title='Temperatura')
@@ -144,55 +158,87 @@ class VentanaPrincipal(QMainWindow):
 		print(str(rx,'utf-8').strip())
 		x = str(rx,'utf-8').strip().split(',')
 		data_from_arduino = []
-		for data in x:
-			if data == '0.000000\x00':
-				data_from_arduino.append(0.000000)
-			else:
-				data_from_arduino.append(float(data))
-			print(data)
+		data_from_arduino_antena_2 = []
+		if len(x) == 3:
+			for data in x:
+				data_from_arduino_antena_2.append(float(data))
+				self.data_from_arduino_estatico_antena_2 = data_from_arduino_antena_2  
+		else:
+			for data in x:
+				data_from_arduino.append(float(data))	
+			#data_from_arduino_antena_2.append(2)
+			#data_from_arduino_antena_2.append(10)	
+			self.dato_arduino_estatico = data_from_arduino
 	
-		self.x_coord_label_subida.setText(str(data_from_arduino[0])+self.aceleracion_si )
-		self.y_coord_label_subida.setText(str(data_from_arduino[1])+self.aceleracion_si )
-		self.z_coord_label_subida.setText(str(data_from_arduino[2])+self.aceleracion_si )
-		self.x_coord_label_caida.setText(str(data_from_arduino[0])+self.aceleracion_si )
-		self.y_coord_label_caida.setText(str(data_from_arduino[1])+self.aceleracion_si )
-		self.z_coord_label_caida.setText(str(data_from_arduino[2])+self.aceleracion_si )
-		self.x_coord_label.setText(str(data_from_arduino[9]))
-		self.y_coord_label.setText(str(data_from_arduino[10]))
-		self.yaw_label.setText(str(data_from_arduino[3])+self.grados_si)
-		self.pitch_label.setText(str(data_from_arduino[4])+self.grados_si)
-		self.roll_label.setText(str(data_from_arduino[5])+self.grados_si)
-		self.temperatura_label.setText(str(data_from_arduino[6])+self.temperatura_si)
-		self.presion_label.setText(str(data_from_arduino[7])+self.presion)
-		self.altura_label.setText(str(data_from_arduino[8])+self.distancia_si)
-		self.altura_max_label.setText(str(data_from_arduino[8])+self.distancia_si)
+	
+		self.x_coord_label_subida.setText(str(self.dato_arduino_estatico[0])+self.aceleracion_si )
+		self.y_coord_label_subida.setText(str(self.dato_arduino_estatico[1])+self.aceleracion_si )
+		self.z_coord_label_subida.setText(str(self.dato_arduino_estatico[2])+self.aceleracion_si )
+		self.x_coord_label_caida.setText(str(self.dato_arduino_estatico[0])+self.aceleracion_si )
+		self.y_coord_label_caida.setText(str(self.dato_arduino_estatico[1])+self.aceleracion_si )
+		self.z_coord_label_caida.setText(str(self.dato_arduino_estatico[2])+self.aceleracion_si )
+		self.x_coord_label.setText(str(self.dato_arduino_estatico[9]))
+		self.y_coord_label.setText(str(self.dato_arduino_estatico[10]))
+		self.yaw_label.setText(str(self.dato_arduino_estatico[3])+self.grados_si)
+		self.pitch_label.setText(str(self.dato_arduino_estatico[4])+self.grados_si)
+		self.roll_label.setText(str(self.dato_arduino_estatico[5])+self.grados_si)
+		self.temperatura_label.setText(str(self.dato_arduino_estatico[6])+self.temperatura_si)
+		self.presion_label.setText(str(self.dato_arduino_estatico[7])+self.presion_si)
+		self.altura_label.setText(str(self.dato_arduino_estatico[8])+self.distancia_si)
+		self.altura_max_label.setText(str(self.dato_arduino_estatico[8])+self.distancia_si)
 		global tiempo_inicio
 		tiempo_trasncurrido = time.time() - tiempo_inicio
-		velocidad_maxima = data_from_arduino[8]/tiempo_trasncurrido
-		self.vel_max_label.setText(str(velocidad_maxima)+self.velocidad_si)
-		distancia = self.radio_tierra * np.arccos(np.sin(data_from_arduino[9])*np.sin(data_from_arduino[9])
-											+(np.abs(data_from_arduino[10]+data_from_arduino[10])
-											*np.cos(data_from_arduino[10])*np.cos(data_from_arduino[10])))
-		self.distancia_label.setText(distancia+self.distancia_si)
+		velocidad_maxima = self.dato_arduino_estatico[8]/tiempo_trasncurrido
+		# velocidad_maxima = "{:.4f}".format(velocidad_maxima_arduino)
+		self.vel_max_label.setText(str("{:.4f}".format(velocidad_maxima))+self.velocidad_si)
 		
-		self.y = self.y[1:]
-		self.y.append(data_from_arduino)
+		if len(self.data_from_arduino_estatico_antena_2) != 0:				
+			distancia = self.radio_tierra * np.arccos(np.sin(self.dato_arduino_estatico[9])*np.sin(self.data_from_arduino_estatico_antena_2[0])
+												+(np.abs(self.dato_arduino_estatico[10]+self.data_from_arduino_estatico_antena_2[1])
+												*np.cos(self.dato_arduino_estatico[10])*np.cos(self.data_from_arduino_estatico_antena_2[1])))
+		else:
+			distancia = 0
+		self.distancia_label.setText(str(distancia)+self.distancia_si)
+		
+		self.altura_data = self.altura_data[1:]
+		self.presion_data = self.presion_data[1:]
+		self.temperatura_data = self.temperatura_data[1:]
+		self.yaw = self.yaw[1:]
+		self.pitch = self.pitch[1:]
+		self.roll = self.roll[1:]
+		self.aceleracion_x = self.aceleracion_x[1:]
+		self.aceleracion_y = self.aceleracion_y[1:]
+		self.aceleracion_z = self.aceleracion_z[1:]
+		self.latitud = self.latitud[1:]
+		self.longitud = self.longitud[1:]
+
+		self.altura_data.append(self.dato_arduino_estatico[8])
+		self.presion_data.append(self.dato_arduino_estatico[7])
+		self.temperatura_data.append(self.dato_arduino_estatico[6])
+		self.yaw.append(self.dato_arduino_estatico[3])
+		self.pitch.append(self.dato_arduino_estatico[4])
+		self.roll.append(self.dato_arduino_estatico[5])
+		self.aceleracion_x.append(self.dato_arduino_estatico[0])
+		self.aceleracion_y.append(self.dato_arduino_estatico[1])
+		self.aceleracion_z.append(self.dato_arduino_estatico[2])
+		self.latitud.append(self.dato_arduino_estatico[9])
+		self.longitud.append(self.dato_arduino_estatico[10])
 
 		self.gfc_temperatura.clear()
-		self.gfc_temperatura.plot(self.x,self.y, color='red')
+		self.gfc_temperatura.plot(self.x,self.temperatura_data, color='red')
 
 		
 		self.gfc_presion.clear()
-		self.gfc_presion.plot(self.x,self.y, color='red')
+		self.gfc_presion.plot(self.x,self.presion_data, color='red')
 
 		
 		self.gfc_altura.clear()
-		self.gfc_altura.plot(self.x,self.y, color='red')
+		self.gfc_altura.plot(self.x,self.altura_data, color='red')
 
 		self.gfc_angulo.clear()
-		self.gfc_angulo.plot(self.x, np.sin(self.y), pen='r', name='Sin(x)')
-		self.gfc_angulo.plot(self.x, np.cos(self.y), pen='g', name='Cos(x)')
-		self.gfc_angulo.plot(self.x, np.tan(self.y), pen='b', name='Tan(x)')
+		self.gfc_angulo.plot(self.x, self.yaw, pen='r', name='Yaw')
+		self.gfc_angulo.plot(self.x, self.pitch, pen='g', name='Pitch')
+		self.gfc_angulo.plot(self.x, self.roll, pen='b', name='Roll')
 
 		axis_x = gl.GLLinePlotItem(pos=np.array([[0, 0, 0], [100, 0, 0]]), color=(1.0, 0.0, 0.0, 1.0), width=3)
 		axis_y = gl.GLLinePlotItem(pos=np.array([[0, 0, 0], [0, 100, 0]]), color=(0.0, 1.0, 0.0, 1.0), width=3)
@@ -202,9 +248,9 @@ class VentanaPrincipal(QMainWindow):
 		self.gfc_aceleracion_subida.addItem(axis_z)
         
         # Creamos datos de ejemplo para la línea 3D
-		x = self.y
-		y = self.y
-		z = self.y
+		x = list(np.linspace(0,0,100))
+		y = list(np.linspace(0,0,100))
+		z = self.aceleracion_z
         
         # Añadimos la línea a la visualización 3D
 		self.line_plot = gl.GLLinePlotItem(pos=np.column_stack((x, y, z)), color=(1.0, 1.0, 1.0, 1.0), width=3)
@@ -219,9 +265,9 @@ class VentanaPrincipal(QMainWindow):
 		self.gfc_aceleracion_caida.addItem(axis_z2)
         
         # Creamos datos de ejemplo para la línea 3D
-		x = self.y
-		y = self.y
-		z = self.y
+		x = self.aceleracion_x
+		y = self.aceleracion_y
+		z = self.aceleracion_z
         
         # Añadimos la línea a la visualización 3D
 		self.line_plot = gl.GLLinePlotItem(pos=np.column_stack((x, y, z)), color=(1.0, 1.0, 1.0, 1.0), width=3)
